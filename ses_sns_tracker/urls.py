71119e00767467dac1f70baa87b2ad7642179097
ferdinand-hoffmann <f.hoffmann@anfe.ma>
2020-01-28 16:26:57 +0100

    Add app_name to urls.py

--- ses_sns_tracker/urls.py
+++ ses_sns_tracker/urls.py
@@ -6,6 +6,8 @@ from django.views.decorators.csrf import csrf_exempt
 from django_ses.views import handle_bounce
 
 
+app_name = 'ses_sns_tracker'
+
 urlpatterns = [
     url(r'^bounce/$', csrf_exempt(handle_bounce)),
 ]
