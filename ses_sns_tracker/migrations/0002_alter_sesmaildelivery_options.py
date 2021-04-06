657667c5c45c1678f5200c14dd13cc2914686cb9
Ferdinand Hoffmann <f.hoffmann@anfe.ma>
2021-04-06 17:27:21 +0200

    Add missing migration

---
+++ ses_sns_tracker/migrations/0002_alter_sesmaildelivery_options.py
@@ -0,0 +1,15 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('ses_sns_tracker', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='sesmaildelivery',
+            options={'verbose_name': 'SES Mail Delivery', 'verbose_name_plural': 'SES Mail Deliveries'},
+        ),
+    ]
