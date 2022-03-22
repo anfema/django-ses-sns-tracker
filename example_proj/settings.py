6fb9a4a8dc0c32aa2354bcc99e68a52c389d7301
Richard <richard@stromer.org>
2022-03-22 10:48:53 +0100

    configure default auto field in AppConfig instead of settings (make it more generic for other projects)

--- example_proj/settings.py
+++ example_proj/settings.py
@@ -88,8 +88,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
--- ses_sns_tracker/apps.py
+++ ses_sns_tracker/apps.py
@@ -4,6 +4,7 @@ from django.apps import AppConfig
 class SESSNSTrackerConfig(AppConfig):
     name = 'ses_sns_tracker'
     verbose_name = "SES/SNS Tracker"
+    default_auto_field = 'django.db.models.AutoField'
 
     def ready(self):
         from . import signals  # NOQA
