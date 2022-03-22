fccb2bd02f6542e4937439cfc5b0398b9e522a13
Richard <richard@stromer.org>
2022-03-22 10:44:14 +0100

    configure DEFAULT_AUTO_FIELD setting

--- example_proj/settings.py
+++ example_proj/settings.py
@@ -88,6 +88,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
