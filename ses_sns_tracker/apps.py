5193805ae7e343d099ced69dfb4e1c3ec1e8b0f6
Richard <richard@stromer.org>
2022-03-22 10:57:45 +0100

    adopt django 3 default BigAutoField for default_auto_field

--- ses_sns_tracker/apps.py
+++ ses_sns_tracker/apps.py
@@ -4,7 +4,7 @@ from django.apps import AppConfig
 class SESSNSTrackerConfig(AppConfig):
     name = 'ses_sns_tracker'
     verbose_name = "SES/SNS Tracker"
-    default_auto_field = 'django.db.models.AutoField'
+    default_auto_field = 'django.db.models.BigAutoField'
 
     def ready(self):
         from . import signals  # NOQA
