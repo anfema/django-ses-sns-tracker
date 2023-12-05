9be36cd9671dac9a0c41bbd5b35f9e938a0d9d41
Ferdinand Hoffmann <f.hoffmann@anfe.ma>
2023-12-05 14:14:14 +0100

    Fix code style warning & reformat migration files

--- ses_sns_tracker/migrations/0001_initial.py
+++ ses_sns_tracker/migrations/0001_initial.py
@@ -5,30 +5,43 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='SESMailDelivery',
+            name="SESMailDelivery",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('recipient', models.EmailField(max_length=254)),
-                ('message_id', models.CharField(max_length=128)),
-                ('request_id', models.CharField(max_length=128)),
-                ('state', models.PositiveSmallIntegerField(choices=[(0, 'Sent'), (1, 'Delivered'), (2, 'Bounced'), (3, 'Complaint')], default=0)),
-                ('state_data', django.contrib.postgres.fields.jsonb.JSONField(blank=True, default=dict, help_text='Amazon SNS event data (bounce/complaint/delivery object)')),
-                ('mail_data', django.contrib.postgres.fields.jsonb.JSONField(blank=True, default=dict, help_text='Amazon SNS mail data')),
-                ('sent_at', models.DateTimeField(auto_now_add=True)),
-                ('updated_at', models.DateTimeField(auto_now=True)),
+                ("id", models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name="ID")),
+                ("recipient", models.EmailField(max_length=254)),
+                ("message_id", models.CharField(max_length=128)),
+                ("request_id", models.CharField(max_length=128)),
+                (
+                    "state",
+                    models.PositiveSmallIntegerField(
+                        choices=[(0, "Sent"), (1, "Delivered"), (2, "Bounced"), (3, "Complaint")], default=0
+                    ),
+                ),
+                (
+                    "state_data",
+                    django.contrib.postgres.fields.jsonb.JSONField(
+                        blank=True, default=dict, help_text="Amazon SNS event data (bounce/complaint/delivery object)"
+                    ),
+                ),
+                (
+                    "mail_data",
+                    django.contrib.postgres.fields.jsonb.JSONField(
+                        blank=True, default=dict, help_text="Amazon SNS mail data"
+                    ),
+                ),
+                ("sent_at", models.DateTimeField(auto_now_add=True)),
+                ("updated_at", models.DateTimeField(auto_now=True)),
             ],
             options={
-                'ordering': ('-updated_at',),
-                'verbose_name': 'SES Mail Delivery',
-                'verbose_name_plural': 'SES Mail Deliveries',
+                "ordering": ("-updated_at",),
+                "verbose_name": "SES Mail Delivery",
+                "verbose_name_plural": "SES Mail Deliveries",
             },
         ),
     ]
--- ses_sns_tracker/migrations/0004_alter_sesmaildelivery_id.py
+++ ses_sns_tracker/migrations/0004_alter_sesmaildelivery_id.py
@@ -4,15 +4,14 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('ses_sns_tracker', '0003_auto_20220322_0945'),
+        ("ses_sns_tracker", "0003_auto_20220322_0945"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='sesmaildelivery',
-            name='id',
-            field=models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID'),
+            model_name="sesmaildelivery",
+            name="id",
+            field=models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name="ID"),
         ),
     ]
